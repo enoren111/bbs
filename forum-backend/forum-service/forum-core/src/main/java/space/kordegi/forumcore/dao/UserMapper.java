bad12799c10055a76ff4f0dea18c24cd5ae21fb9
Eno Ren <enoren@163.com>
2024-03-28 10:38:30 -0700

    The login function can connect the backend and frontend.

--- forum-backend/forum-service/forum-core/src/main/java/space/kordegi/forumcore/dao/UserMapper.java
+++ forum-backend/forum-service/forum-core/src/main/java/space/kordegi/forumcore/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package space.kordegi.forumcore.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import space.kordegi.forumcore.entity.User;
 
@@ -11,4 +12,8 @@ public interface UserMapper {
   @Select("SELECT *\n" +
     "\tFROM public.users;")
   User getUser();
+
+  @Select("SELECT password FROM public.users where name = 'John Doe';")
+  String getPassword();
 }
+
--- forum-backend/forum-service/forum-core/src/main/java/space/kordegi/forumcore/service/UserService.java
+++ forum-backend/forum-service/forum-core/src/main/java/space/kordegi/forumcore/service/UserService.java
@@ -2,6 +2,7 @@ package space.kordegi.forumcore.service;
 
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import space.kordegi.forumcore.dao.LoginMapper;
 import space.kordegi.forumcore.dao.UserMapper;
 import space.kordegi.forumcore.entity.User;
 
@@ -10,7 +11,12 @@ public class UserService {
   @Resource
   private UserMapper userMapper;
 
+  /**
+   * this function is used to find the login username*/
   public User getUser(){
     return userMapper.getUser();
   }
+
+  public String getPassword(){return  userMapper.getPassword();}
 }
+
